3a0cb90ef8ce1f52a30b294c00cf0cf9e9adaaf8
Yuto Nakamura <maron07222010@gmail.com>
2023-04-28 22:36:34 +0900

    extern useをやめた

--- bind-v4l2/build.rs
+++ bind-v4l2/build.rs
@@ -1,15 +1,15 @@
-extern crate bindgen;
+use bindgen;
 
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let bindings = bindgen::Builder::default()
+    let bindings: bindgen::Bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .generate()
         .expect("Failed to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_path: PathBuf = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("v4l2_bindings.rs"))
         .expect("Failed to write bindings");
